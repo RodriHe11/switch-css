e767754ba1fcbb7ee32a319291f8a8981472ae4d
Israel Rodrigo <israel.code.rodrigo.11@gmail.com>
2023-07-01 11:32:18 +0200

    Funcionalidad Titulo Switch

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const btnStart=document.getElementById("start"),
       pantalla2=document.querySelector(".pantallaLogo"),
       pantalla3=document.querySelector(".pantallaLogo2"),
       btnAccion=document.querySelector(".btnMenos");
-      btnAc=document.querySelector(".accion");
+      btnAc=document.querySelector(".accion"),
+      switchDrch=document.querySelector(".switchDrch"),
+      tituloSwitch=document.querySelector(".tituloSwitch");
 
 
 document.addEventListener("click",e=>{
@@ -20,7 +22,7 @@ document.addEventListener("click",e=>{
     }
 });
 
-function encender(){
+/* async function encender(){
     btnAc.classList.add("active");
     btnAccion.style.cssText="justify-content:flex-end";
     btnAc.style.cssText="background-color:var(--activo)";
@@ -31,12 +33,52 @@ function encender(){
                 pantalla2.classList.toggle("ocultar");
                 temporizador=setTimeout(e=>{
                     pantalla3.classList.toggle("mostrar");
+                    setTimeout(e=>{
+                        switchDrch.classList.add("traslateIzq");
+                        setTimeout(e=>{
+                            tituloSwitch.classList.remove("none");
+                        },500);
+                    },500);
                     temporizador=setTimeout(e=>{
                         pantalla3.classList.toggle("ocultar");
-                    },3000);
-                },3000);
-            },3000);
-        },3000);
+                    },3500);
+                },3500);
+            },3500);
+        },3500);
+} */
+async function encender(){
+    btnAc.classList.add("active");
+    btnAccion.style.cssText="justify-content:flex-end";
+    btnAc.style.cssText="background-color:var(--activo)";
+    pantalla1.classList.toggle("ocultar");
+
+    await esperar(3500);
+    pantalla2.classList.toggle("mostrar");
+    console.log("tiempo finalizado");
+
+    await esperar(3500);
+    pantalla2.classList.toggle("ocultar");
+    console.log("tiempo finalizado");
+
+    await esperar(500);
+    pantalla3.classList.toggle("mostrar");
+    console.log("tiempo finalizado");
+
+    await esperar(500);
+    switchDrch.classList.add("traslateIzq");
+    console.log("tiempo finalizado");
+
+    await esperar(500);
+    tituloSwitch.classList.remove("none");
+    console.log("tiempo finalizado");
+
+    await esperar(3000);
+    pantalla3.classList.toggle("ocultar");
+    console.log("tiempo finalizado");
+}
+
+function esperar(tiempo){
+    return new Promise(resolve => temporizador=setTimeout(resolve,tiempo));
 }
 function apagar(){
     btnAc.classList.remove("active");
